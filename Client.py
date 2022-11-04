8208bbc54cfca8b529cafbeb3ec474e358c969f8
andrewss <andrewss@msoe.edu>
2022-11-04 11:26:02 -0500

    changed forever loop

--- Client.py
+++ Client.py
@@ -14,25 +14,24 @@ def tcp_send(server_host, server_port):
     tcp_socket.connect((server_host, server_port))
     # TODO: Fix listener lines
 
-    # listen_address = ('', port)
-    # server_socket.bind(listen_address)
-    # server_socket.listen(num_connections)
-    # Display any and all possible options the user could select
-    # Create a file(messages) with the options
-    header = 'Key (required),artist(optional),song(optional),number of songs(optional),album(optional),' + 'genres(optional)'
-    optionOneDescription = 'Select a single song = 1 \r\n'
-    optionTwoDescription = 'Select a certain number of specified songs = 2 \r\n'
-    optionThreeDescription = 'Select a random number of songs = 3 \r\n'
-    optionFourDescription = 'Select a specific album = 4 \r\n'
-    optionFiveDescription = 'Select all songs of a specific artist = 5 \r\n'
-    optionSixDescription = 'Select songs from a specific genre = 6 \r\n'
-    totalDescription = optionOneDescription + optionTwoDescription + optionThreeDescription + optionFourDescription + optionFiveDescription + optionSixDescription
-    askWhatOption = 'Which option would you like to select: \r\n\r\n'
-    print(totalDescription + askWhatOption)
-    userRequest = int(input())
-    # TODO: Add a listener
-
     while True:
+        # listen_address = ('', port)
+        # server_socket.bind(listen_address)
+        # server_socket.listen(num_connections)
+        # Display any and all possible options the user could select
+        # Create a file(messages) with the options
+        header = 'Key (required),artist(optional),song(optional),number of songs(optional),album(optional),' + 'genres(optional)'
+        optionOneDescription = 'Select a single song = 1 \r\n'
+        optionTwoDescription = 'Select a certain number of specified songs = 2 \r\n'
+        optionThreeDescription = 'Select a random number of songs = 3 \r\n'
+        optionFourDescription = 'Select a specific album = 4 \r\n'
+        optionFiveDescription = 'Select all songs of a specific artist = 5 \r\n'
+        optionSixDescription = 'Select songs from a specific genre = 6 \r\n'
+        totalDescription = optionOneDescription + optionTwoDescription + optionThreeDescription + optionFourDescription + optionFiveDescription + optionSixDescription
+        askWhatOption = 'Which option would you like to select: \r\n\r\n'
+        print(totalDescription + askWhatOption)
+        userRequest = int(input())
+        # TODO: Add a listener
         if userRequest == 1:
             print('What artist would you like to play: ')
             artistans = input()
