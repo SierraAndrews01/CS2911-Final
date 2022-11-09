092940ee5ad6c017f4425c5c3dfb757a29db6f56
andrewss <andrewss@msoe.edu>
2022-11-09 12:28:53 -0600

    song trials

--- Client.py
+++ Client.py
@@ -52,14 +52,13 @@ def tcp_send(server_host, server_port):
             songans = input()
             tcp_socket.sendall(b'1 \r\n artist: ' + artistans.encode() + b'\r\n song: ' + songans.encode() + b'\r\n\r\n')
             song = b'' + reciveUntilEnd(tcp_socket)
-            song = song.decode()
             print(song)
 
             # Open file in binary write mode
             binary_file = open("my_file.flac", "wb")
             # Write bytes to file
             print(type(song))
-            binary_file.writelines(song.encode())
+            binary_file.writelines(song)
             # Close file
             binary_file.close()
             # Play song
